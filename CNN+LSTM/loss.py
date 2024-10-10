Fixed training instability by adding Batch Normalization

CNN+LSTM/loss.py
@@ -10,8 +10,8 @@ def half_min_distance(feat_means):
         # Exclude the current class feature mean
         excluded_feat_means = torch.concat((feat_means[:i], feat_means[i+1:]))
         diff =  excluded_feat_means - feat_means[i]
-        d = torch.linalg.norm(diff, dim=1, dtype=torch.float64)
-        half_min_distance[i] = d.detach().min()
+        d = torch.linalg.norm(diff, dim=1, dtype=torch.float64, ord=2)
+        half_min_distance[i] = d.detach().min() / 2
     return half_min_distance
 
 def loss_function(gt_exp_class, gt_exp_state, pred_exp_class, pred_exp_state, class_feat_mean, sample_feat, half_min_distance):
@@ -32,7 +32,7 @@ def loss_function(gt_exp_class, gt_exp_state, pred_exp_class, pred_exp_state, cl
     # E2 Intra class variation
     sample_class_feat_mean = torch.matmul(gt_exp_class.float(), class_feat_mean) # We do this so that each sample feature of class c will be subtracted by corresponding feature mean of the same class c
     sample_hmd = torch.matmul(gt_exp_class.float(), half_min_distance) # Same thing with half minimum distance, each sample will operate with its corresponding HMD of the same class as sample ground truth class
-    d = torch.linalg.norm(sample_feat - sample_class_feat_mean, dim=1, dtype=torch.float64).pow(2) - sample_hmd.pow(2)
+    d = torch.linalg.norm(sample_feat - sample_class_feat_mean, dim=1, dtype=torch.float64, ord=2).pow(2) - sample_hmd.pow(2)
     beta = 1 # Sharpness parameter
     E2 = softplus(d).sum() / 2
     # TODO: Debug E2 value too big

CNN+LSTM/model.py
@@ -11,12 +11,15 @@ class MicroExpressionCNN(torch.nn.Module):
         self.conv_layer = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=0, stride=1),
             torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(num_features=32),
             torch.nn.MaxPool2d(kernel_size=3, stride=2),
             torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=0, stride=1),
             torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(num_features=64),
             torch.nn.MaxPool2d(kernel_size=3, stride=2),
             torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=0, stride=1),
             torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(num_features=64),
             torch.nn.MaxPool2d(kernel_size=3, stride=2),
         )
 
@@ -24,17 +27,19 @@ class MicroExpressionCNN(torch.nn.Module):
             torch.nn.Flatten(),
             torch.nn.LazyLinear(out_features=512), # infer input feature from output of convolution layer
             torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(num_features=512),
             torch.nn.Linear(in_features=512, out_features=512),
             torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(num_features=512),
         )
 
         self.expression_classifier = torch.nn.Sequential(
             torch.nn.Linear(in_features=512, out_features=self.exp_class_size),
-            torch.nn.Softmax(),
+            torch.nn.Softmax(dim=1),
         )
         self.expression_state_classifier = torch.nn.Sequential(
             torch.nn.Linear(in_features=512, out_features=self.exp_state_size),
-            torch.nn.Softmax(),
+            torch.nn.Softmax(dim=1),
         )
 
 

CNN+LSTM/train.py
@@ -5,18 +5,23 @@ from config import BATCH_SIZE, EPOCH, EXP_STATE_SIZE, EXP_CLASS_SIZE, LEARNING_R
 from loss import loss_function, half_min_distance
 from model import MicroExpressionCNN
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def main():
+    cuda = torch.cuda.current_device()
+    if (torch.cuda.is_available()):
+        torch.set_default_device(cuda)
     dataset = MicroExpressionDataset("D:\\CASME2\\label.csv", "D:\\CASME2")
-    train_dataset, test_dataset = random_split(dataset, [0.85, 0.15])
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    train_dataset, test_dataset = random_split(dataset, [0.85, 0.15], generator=torch.Generator(device=cuda))
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, generator=torch.Generator(device=cuda))
+    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True, generator=torch.Generator(device=cuda))
 
     model = MicroExpressionCNN(EXP_CLASS_SIZE, EXP_STATE_SIZE)
     model.train()
 
     for epoch in range(EPOCH):
         print(f"Epoch {epoch + 1}\n_____________________________________")
+        # TODO: Calculate Spatial Feature mean and Half Minimum Distance every epoch instead of every batch
         train_loop(train_dataloader, model)
 
 def train_loop(train_dataloader, model):
@@ -26,11 +31,18 @@ def train_loop(train_dataloader, model):
 
     optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9)
     for batch, (X, y_class, y_state) in enumerate(train_dataloader):
-        # Compute prediction and loss
+
+        # Move to GPU
+        X = X.to(device)
+        y_class = y_class.to(device)
+        y_state = y_state.to(device)
+
+        # Compute expression class and state prediction along with spatial feature vector
         feat, pred_class, pred_state = model(X.float())
+        # TODO: Figure out why predicted predictive probability got pushed to [1.00 1.00 ... 1.00] as we trained the model
 
         # Using y_class, we know which ground truth class that each sample belong to
-        class_feat_means = torch.zeros((EXP_CLASS_SIZE, 512))
+        class_feat_means = torch.zeros((EXP_CLASS_SIZE, 512), device=device)
         for k in range(EXP_CLASS_SIZE):
             class_k_index = y_class[:, k]
             class_k_feat_mean = feat.index_select(dim=0, index=class_k_index).mean(dim=0)
@@ -44,9 +56,13 @@ def train_loop(train_dataloader, model):
         # Backpropagation
         loss.backward()
         optimizer.step()
+
+        # for p in model.parameters():
+        #     print(p.grad.norm())
+
         optimizer.zero_grad()
 
-        if batch % 100 == 0:
+        if batch % 10 == 0:
             loss, current = loss.item(), batch * BATCH_SIZE + len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 