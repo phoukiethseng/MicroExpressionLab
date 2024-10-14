1c8394f5c5a1cab9efeedfc002088f47fe23d332
Phou Kiethseng <puseng123@gmail.com>
2024-10-14 21:16:44 +0800

    Small bug fixes and added E4 error term to loss function

--- CNN+LSTM/loss.py
+++ CNN+LSTM/loss.py
@@ -2,7 +2,7 @@ import torch
 from model import MicroExpressionCNN
 from config import *
 
-softplus = torch.nn.Softplus()
+softplus = torch.nn.Softplus() # Smoothed approximation of ReLU
 
 def half_min_distance(feat_means):
     half_min_distance = torch.zeros(EXP_CLASS_SIZE)
@@ -14,12 +14,12 @@ def half_min_distance(feat_means):
         half_min_distance[i] = d.detach().min() / 2
     return half_min_distance
 
-def loss_function(gt_exp_class, gt_exp_state, pred_exp_class, pred_exp_state, class_feat_mean, sample_feat, half_min_distance):
+def loss_function(gt_exp_class, gt_exp_state, pred_exp_class, pred_exp_state, class_feature_means, class_state_feature_means, sample_feat, half_min_distance):
     """
 
     :param half_min_distance: half_min_distance[i] is half distance between feature mean i and closest feature mean
     :param sample_feat: Extracted feature from last fully connected layer, has shape of (batch_size, feature_size)
-    :param class_feat_mean: Spatial feature vector mean of all training examples for all class (Has to be updated every beginning of Epoch), has shape of (7, feature_size)
+    :param class_feature_means: Spatial feature vector mean of all training examples for all class (Has to be updated every beginning of Epoch), has shape of (7, feature_size)
     :param gt_exp_class: Expression Class ground truth, it should have shape of (batch_size, EXP_CLASS_SIZE), which gt_exp_class[i,j] = 1 means i sample contain expression class j
     :param gt_exp_state: Expression State ground truth, it should have shape of (batch_size, EXP_STATE_SIZE), which gt_exp_state[i,j] = 1 means i sample contain expression state j
     :param pred_exp_class: Expression Class predicted probability, it should have shape of (batch_size, EXP_CLASS_SIZE), which gt_exp_class[i,j] = 0.7 means i sample has probability 0.7 of being class j
@@ -30,17 +30,29 @@ def loss_function(gt_exp_class, gt_exp_state, pred_exp_class, pred_exp_state, cl
     E1  = - (gt_exp_class * torch.log(pred_exp_class + 1e-5)).sum() # we add predicted probability with 1e-5 to avoid probability being zero, which will cause log to goes to minus infinity
 
     # E2 Intra class variation
-    sample_class_feat_mean = torch.matmul(gt_exp_class.float(), class_feat_mean) # We do this so that each sample feature of class c will be subtracted by corresponding feature mean of the same class c
+    sample_class_feat_mean = torch.matmul(gt_exp_class.float(), class_feature_means) # We do this so that each sample feature of class c will be subtracted by corresponding feature mean of the same class c
     sample_hmd = torch.matmul(gt_exp_class.float(), half_min_distance) # Same thing with half minimum distance, each sample will operate with its corresponding HMD of the same class as sample ground truth class
     d = torch.linalg.norm(sample_feat - sample_class_feat_mean, dim=1, dtype=torch.float64, ord=2).pow(2) - sample_hmd.pow(2)
     beta = 1 # Sharpness parameter
     E2 = softplus(d).sum() / 2
-    # TODO: Debug E2 value too big
 
     # E3 expression state classification error
     E3 = - (gt_exp_state * torch.log(pred_exp_state + 1e-5)).sum()
 
-    return E1 + E2 + E3
+    # E4 Intra Expression state variation
+    E4 = 0
+    # class_index = torch.matmul(gt_exp_class.float(), torch.arange(0, EXP_CLASS_SIZE, requires_grad=False, dtype=torch.float32))
+    # for k in range(EXP_CLASS_SIZE):
+    #     index = class_index == k
+    #     # Feature means of every expression state of class k
+    #     sample_class_k_state_feat_mean = torch.matmul(gt_exp_state[index, :].float(), class_state_feature_means[k, :, :])
+    #     d =torch.linalg.norm(sample_feat[index, :] - sample_class_k_state_feat_mean, dim=1, dtype=torch.float64, ord=2)
+    #     t = 1
+    #     e_k = softplus(d.pow(2) - (half_min_distance[k] / t).pow(2)).sum(dim=0)
+    #     E4 += e_k
+    # E4 /= 2
+
+    return E1 + E2 + E3 + E4
 
 def test_loss_function():
 
--- CNN+LSTM/train.py
+++ CNN+LSTM/train.py
@@ -1,11 +1,15 @@
+from sympy.stats.sampling.sample_numpy import numpy
+
 from dataset import MicroExpressionDataset
 import torch
 from torch.utils.data import DataLoader, random_split
 from config import BATCH_SIZE, EPOCH, EXP_STATE_SIZE, EXP_CLASS_SIZE, LEARNING_RATE
 from loss import loss_function, half_min_distance
 from model import MicroExpressionCNN
+from datetime import datetime
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+worker_size = 6
 
 def main():
     cuda = torch.cuda.current_device()
@@ -13,8 +17,12 @@ def main():
         torch.set_default_device(cuda)
     dataset = MicroExpressionDataset("D:\\CASME2\\label.csv", "D:\\CASME2")
     train_dataset, test_dataset = random_split(dataset, [0.85, 0.15], generator=torch.Generator(device=cuda))
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, generator=torch.Generator(device=cuda))
-    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True, generator=torch.Generator(device=cuda))
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True,
+                                  generator=torch.Generator(device=cuda), num_workers=worker_size)
+
+    epoch_dataloader_batch_size = 2000
+    epoch_dataloader = DataLoader(train_dataset, batch_size=epoch_dataloader_batch_size, shuffle=False,
+                                  generator=torch.Generator(device=cuda), num_workers=worker_size)
 
     model = MicroExpressionCNN(EXP_CLASS_SIZE, EXP_STATE_SIZE)
     model.train()
@@ -22,12 +30,62 @@ def main():
     for epoch in range(EPOCH):
         print(f"Epoch {epoch + 1}\n_____________________________________")
         # TODO: Calculate Spatial Feature mean and Half Minimum Distance every epoch instead of every batch
-        train_loop(train_dataloader, model)
+        print(
+            'Calculating feature mean of Expression Class and State and Half-Minimum-Distance'
+            'between Feature mean at the start of epoch...')
+        class_feature_means, class_state_feature_means = get_exp_class_feature_mean(model, epoch_dataloader,
+                                                                                    batch_size=epoch_dataloader_batch_size)
+        hmd = half_min_distance(class_feature_means)
+        print('Done')
+        print(f'half minimum distance = {hmd}\nfeature mean = {class_feature_means}')
+        train_loop(train_dataloader, model, class_feature_means, hmd, class_state_feature_means)
 
-def train_loop(train_dataloader, model):
+    # Save the model
+    torch.save(model.state_dict(), f"D:\\CASME2\\{datetime.now().strftime('%Y-%m-%d_%H-%M')}.pt")
+
+
+def get_exp_class_feature_mean(model, dataloader, batch_size=128):
+    class_feat_means = torch.zeros((EXP_CLASS_SIZE, 512), device=device, requires_grad=False)
+    class_state_feat_means = torch.zeros((EXP_CLASS_SIZE, EXP_STATE_SIZE, 512), device=device, requires_grad=False)
+
+    a = torch.arange(0, EXP_CLASS_SIZE, requires_grad=False, dtype=torch.float32, device=device)
+    b = torch.arange(0, EXP_STATE_SIZE, requires_grad=False, dtype=torch.float32, device=device)
+
+    dataset_size = len(dataloader.dataset)
+
+    for batch, (X, y_class, y_state) in enumerate(dataloader):
+
+        if batch * batch_size % 10000 == 0:
+            print(f'Batch: {batch} [{batch * batch_size} / {dataset_size}]')
+
+        X = X.to(device)
+        y_class = y_class.to(device)
+        y_state = y_state.to(device)
+
+        feature, pred_class, pred_state = model(X.float())
+
+        feature = feature.detach()  # Avoid computational graph build up, which will cause increasing memory consumption until out of memory error
+
+        gt_exp_class_index = torch.matmul(y_class.float(), a)
+        gt_exp_state_index = torch.matmul(y_state.float(), b)
+
+        for k in range(EXP_CLASS_SIZE):
+            class_k_index = gt_exp_class_index == k
+            class_feat = feature[class_k_index, :]
+            class_feat_size = class_feat.size()[0]
+            class_feat_means[k] += class_feat.sum(dim=0) / dataset_size
+            # TODO: calculate expression state feature mean
+            for h in range(EXP_STATE_SIZE):
+                state_h_index = torch.logical_and(class_k_index, gt_exp_state_index)
+                class_state_feat = feature[state_h_index, :]
+                class_state_feat_size = class_state_feat.size()[0]
+                class_state_feat_means[k, h] += class_state_feat.sum(dim=0) / dataset_size
+
+    return class_feat_means, class_state_feat_means
+
+
+def train_loop(train_dataloader, model, class_feature_mean, hmd, class_state_feature_means):
     size = len(train_dataloader.dataset)
-    # Set the model to training mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
 
     optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9)
     for batch, (X, y_class, y_state) in enumerate(train_dataloader):
@@ -39,19 +97,10 @@ def train_loop(train_dataloader, model):
 
         # Compute expression class and state prediction along with spatial feature vector
         feat, pred_class, pred_state = model(X.float())
-        # TODO: Figure out why predicted predictive probability got pushed to [1.00 1.00 ... 1.00] as we trained the model
-
-        # Using y_class, we know which ground truth class that each sample belong to
-        class_feat_means = torch.zeros((EXP_CLASS_SIZE, 512), device=device)
-        for k in range(EXP_CLASS_SIZE):
-            class_k_index = y_class[:, k]
-            class_k_feat_mean = feat.index_select(dim=0, index=class_k_index).mean(dim=0)
-            class_feat_means[k] = class_k_feat_mean
-
-        hmd = half_min_distance(class_feat_means)
 
         # Calculate loss
-        loss = loss_function(y_class, y_state, pred_class, pred_state, class_feat_means, feat, hmd)
+        loss = loss_function(y_class, y_state, pred_class, pred_state, class_feature_mean, class_state_feature_means,
+                             feat, hmd)
 
         # Backpropagation
         loss.backward()
@@ -60,11 +109,13 @@ def train_loop(train_dataloader, model):
         # for p in model.parameters():
         #     print(p.grad.norm())
 
+        # Reset gradient
         optimizer.zero_grad()
 
         if batch % 10 == 0:
             loss, current = loss.item(), batch * BATCH_SIZE + len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
 
 if __name__ == '__main__':
     main()
